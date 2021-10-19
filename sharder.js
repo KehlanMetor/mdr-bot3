848641834e0e9336870ab6ea720d0eb43f078295
KehlanMetor <82890272+KehlanMetor@users.noreply.github.com>
2021-10-19 19:08:10 +0200

    Update sharder.js

--- sharder.js
+++ sharder.js
@@ -1,7 +1,7 @@
 const { ShardingManager } = require("discord.js");
 const config = require("./config")
 const manager = new ShardingManager("./index.js", {
-    token: config.token,
+    token: process.env.TOKEN,
     shardArgs: process.argv,
     totalShards: config.shards ? config.shards : "auto",
 });
